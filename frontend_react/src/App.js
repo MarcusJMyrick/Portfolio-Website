590458f8d372d30fece12af28e8bd7375480b859
Marcus Myrick <mjmyrick12@gmail.com>
2022-11-19 12:55:05 -0600

    added index for import

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import About from ".containers/About";
+import Footer from ".containers/Footer";
+import Header from ".containers/Header";
+import Skills from ".containers/Skills";
+import Testimonial from ".containers/Testimonial";
+import About from ".containers/About";
+
 const App = () => {
     return (
         <h1>App</h1>
