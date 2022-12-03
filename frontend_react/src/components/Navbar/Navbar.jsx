3b08ef5c82c0e882385aae00d802b19d41ab3b2c
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-02 21:37:16 -0600

    removed hold logo added new logo and fixed files

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
+import {images} from '../../constants';
 import './Navbar.scss'
 
 const Navbar = () => {
   return (
     <nav>
         <div>
-            <img src={images.logo} alt="logo" />
+            <img src={images.logo1} alt="logo1" />
         </div>
     </nav>
   )
