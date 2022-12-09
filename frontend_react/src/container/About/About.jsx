9da0860c803281473c6dd4a88d64e56e363e3e85
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-08 22:27:50 -0600

    about react script, addition of filler images

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants'
 import './About.scss';
+
+const abouts = [
+    { title: 'Software Engineering', description: 'et is re.', imgUrl: images.about01 },
+    { title: 'Full-Stack Development', description: 'et is re.', imgUrl: images.about02 },
+    { title: 'Backend Engineering', description: 'et is re.', imgUrl: images.about03 },
+    { title: 'Mobile Application Development', description: 'et is re.', imgUrl: images.about04 }
+];
 
 const About = () => {
     return (
-        <div>About</div>
+        <> 
+            <h2 className='head-text'> 
+            Bridging the gap between
+            <br/>
+            <span> Possibilty </span>
+            and
+            <span> Reality </span> 
+            </h2>
+
+            <div className='app__profiles'>
+                {abouts.map((about, index) => (
+                    <motion.div
+                    whileInView= {{ opacity: 1 }}
+                    whileHover= {{ scale: 1.1 }}
+                    transition= {{ duration: 0.5, type: "tween" }}
+                    className= "app__profile-item"
+                    key={about.title + index}
+                    >
+                        <img src={about.imgUrl} alt= {about.title} />
+                        <h2 className= "bold-text" style={{marginTop: 20}}>{about.title}</h2>
+                        <p className= "p-text" style={{marginTop: 10}}>{about.description}</p>
+                    </motion.div>
+                ))}
+            </div>
+        </>     
     );
 }
 
