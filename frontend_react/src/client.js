3e9bd152edf324086f306aa15fe53caff4e5f0fd
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-09 16:07:14 -0600

    client work and env

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,9 +2,13 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from 'sanity/image-url';
 
 export const client = sanityClient ({
-    projectId: 'qgh1i142',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-11-14',
     useCdn: true,
-    token: 'sk1rYKSdlUlj1RFng87qUvYVBkn905gyP8sP8Seprs22DRV6rjZYUXcgZFmqTB1a2mpEveoD0gNMYWY2Zki2P4Q65x5Qv7vuyDQSsbZK2yb4H4vXVDT5kTWHLZsVTPbnpJdRVbipGO9SazsKCOp19cmqqc7lQ4omY5XnBRGkx6Fy34nOiS8L',
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
