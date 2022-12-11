b9a9eaa10abb56eac5f34196aefa019284b27d70
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-11 13:54:32 -0600

    made wrapper container for social media add-oon

---
+++ frontend_react/src/wrapper/AppWrap.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavigationDots, SocialMedia } from '../components'
+
+
+const AppWrap = () => {
+  return (
+    <div>AppWrap</div>
+  )
+}
+
+export default AppWrap
