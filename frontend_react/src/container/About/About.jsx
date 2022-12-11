597cee9997d7f6d7936b0f9b49f60eb64f9e850a
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-10 21:37:08 -0600

    file swapped

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants'
 import './About.scss';
+import { urlFor, client} from '../../client'
 
-const abouts = [
-    { title: 'Software Engineering', description: 'Software Project Description', imgUrl: images.about01 },
-    { title: 'Full-Stack Development', description: 'Software Project Description', imgUrl: images.about02 },
-    { title: 'Backend Engineering', description: 'Software Project Description', imgUrl: images.about03 },
-    { title: 'Mobile Application Development', description: 'Software Project Description', imgUrl: images.about04 }
-];
+
 
 const About = () => {
+const [abouts, setAbouts] = useState([]);
+    
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+        client.fetch(query)
+        .then((data) => setAbouts(data))
+    }, []);
+
     return (
         <> 
             <h2 className='head-text'> 
@@ -31,9 +34,11 @@ const About = () => {
                     className= "app__profile-item"
                     key={about.title + index}
                     >
-                        <img src={about.imgUrl} alt= {about.title} />
+                    
+                        <img src={urlFor(about.imgUrl)} alt= {about.title} />
                         <h2 className= "bold-text" style={{marginTop: 20}}>{about.title}</h2>
                         <p className= "p-text" style={{marginTop: 10}}>{about.description}</p>
+                
                     </motion.div>
                 ))}
             </div>
