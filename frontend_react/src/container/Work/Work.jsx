69f0f7014bca17783cef2214ab2ca19f7d9203a9
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-13 22:42:30 -0600

    change work code

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -67,11 +67,29 @@ const Work = () => {
                       transition={{duration: 0.25 }}
                       className='app__flex'
                     >
-
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                  <a href={work.codeLink} target = " blank " rel = "noreferrer" >
+                    <motion.div
+                      whileInView={{scale: [0,1]}}
+                      whileHover={{ scale: [1, 0.9]}}
+                      transition={{duration: 0.25 }}
+                      className='app__flex'
+                    >
+                      <AiFillGithub />
                     </motion.div>
                   </a>
                 </motion.div>
               </div>
+
+              <div className = "app__work-content app__flex">
+                <h4 className='bold-text'>{work.title}</h4>
+                <p className='p-text' style={{ marginTop: 10}}>{work.description}</p>
+
+                
+              </div>
+
             </div>
           ))}
         </motion.div>
