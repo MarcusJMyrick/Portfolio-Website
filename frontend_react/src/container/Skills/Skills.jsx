dd49b17f6cc101622acdda39b34f6a7b50d71b7b
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-14 19:46:09 -0600

    finished work experience section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -26,7 +26,7 @@ const Skills = () => {
   
     return (
       <>
-        <h2 className="head-text">Skills & Experiences</h2>
+        <h2 className="head-text">Skills & Experience</h2>
   
         <div className="app__skills-container">
           <motion.div className="app__skills-list">
