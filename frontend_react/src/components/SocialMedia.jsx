d052a3f92fbb8d9e67322bf12ef3c606377ab7ca
Marcus Myrick <mjmyrick12@gmail.com>
2022-12-17 18:50:13 -0600

    addition of Social Media links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -6,13 +6,10 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsLinkedin/>
+            <a href = "https://www.linkedin.com/in/marcusjmyrick"><BsLinkedin/></a>
         </div>
         <div>
-            <SiHandshake/>
-        </div>
-        <div>
-            <BsInstagram/>
+           <a href = "https://instagram.com/swe.marcus?igshid=NTdlMDg3MTY="><BsInstagram/></a>
         </div>
     </div>
   )
